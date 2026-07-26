b8b705fe9397b34ad1402321f4d445399617b204
Pierre <pierre.errico@outlook.it>
2026-07-26 15:43:55 +0200

    bug fix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  turbopack: {
+    root: process.cwd(),
+  },
 };
 
 const withMDX = createMDX();
