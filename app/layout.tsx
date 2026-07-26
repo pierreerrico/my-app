a91adbb07c8192d894ef061e5d9106e9c88987a9
Pierre <pierre.errico@outlook.it>
2026-07-26 16:18:49 +0200

    uffffff

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
   viewportFit: "cover",
+  themeColor: "#f2eadb",
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
